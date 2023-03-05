28ac3482223cc6a51da901d5bed10286349b5131
Hamed <jabbary.hamed@gmail.com>
2023-03-05 09:59:17 -0800

    delete books

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
  
  const fetchBooks = async () => {
-       const response = await axios.get("http://localhost:3001/books");
+       const response = await axios.get(`http://localhost:3001/books`);
 
        setBooks(response.data);
  }
@@ -20,10 +20,17 @@ function App() {
    fetchBooks();
  }, []);
 
- const editBookById = (id, newTitle) => {
+ const editBookById = async (id, newTitle) => {
+   
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle
+    });
+
+     
+   
     const updatedBooks =books.map((book) => {
         if(book.id === id) {
-            return { ...book, title: newTitle };
+            return { ...book, ...response.data };
         }
 
         return book;
@@ -33,7 +40,9 @@ function App() {
  }
 
 
- const deleteBookById = (id) => {
+ const deleteBookById = async (id) => {
+      await axios.delete(`http://localhost:3001/books/${id}`);
+
        const updatedBooks = books.filter((book) => book.id !== id);
 
       setBooks(updatedBooks);
